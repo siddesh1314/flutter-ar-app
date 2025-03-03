729b7cd873d093a4c795ca5e34e94eea5a95da53
siddesh1314 <166679843+siddesh1314@users.noreply.github.com>
2025-03-04 00:44:55 +0530

    Added zoom
    Zoom in and out

--- lib/ar_screen.dart
+++ lib/ar_screen.dart
@@ -6,7 +6,6 @@ import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
 import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/material.dart';
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
-import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart';
 
 class ARScreen extends StatefulWidget {
@@ -26,10 +25,10 @@ class _ARScreenState extends State<ARScreen> {
   void updateNode(Vector3 newPosition, Vector3 newScale) {
     if (imageNode != null) {
       setState(() {
-        imageNode!.position = newPosition; // Update position
-        imageNode!.scale = newScale; // Update scale
+        imageNode!.position = newPosition;
+        imageNode!.scale = newScale;
       });
-      print("Node updated: Position: $newPosition, Scale: $newScale");
+      //print("Node updated: Position: $newPosition, Scale: $newScale");
     }
   }
 
@@ -50,56 +49,91 @@ class _ARScreenState extends State<ARScreen> {
             bottom: 20,
             left: 0,
             right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
               children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back, size: 40),
-                  onPressed: () {
-                    if (imageNode != null) {
-                      updateNode(
-                        Vector3(imageNode!.position.x - 0.1,
-                            imageNode!.position.y, imageNode!.position.z),
-                        imageNode!.scale,
-                      );
-                    }
-                  },
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.add, size: 40), // Zoom In
+                      onPressed: () {
+                        if (imageNode != null) {
+                          updateNode(
+                            imageNode!.position,
+                            imageNode!.scale * 1.1, // Inc scale by 10%
+                          );
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.remove, size: 40), // Zoom Out
+                      onPressed: () {
+                        if (imageNode != null) {
+                          updateNode(
+                            imageNode!.position,
+                            imageNode!.scale * 0.9, // Dec scale by 10%
+                          );
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                IconButton(
-                  icon: Icon(Icons.arrow_upward, size: 40),
-                  onPressed: () {
-                    if (imageNode != null) {
-                      updateNode(
-                        Vector3(imageNode!.position.x,
-                            imageNode!.position.y + 0.1, imageNode!.position.z),
-                        imageNode!.scale,
-                      );
-                    }
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.arrow_downward, size: 40),
-                  onPressed: () {
-                    if (imageNode != null) {
-                      updateNode(
-                        Vector3(imageNode!.position.x,
-                            imageNode!.position.y - 0.1, imageNode!.position.z),
-                        imageNode!.scale,
-                      );
-                    }
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.arrow_forward, size: 40),
-                  onPressed: () {
-                    if (imageNode != null) {
-                      updateNode(
-                        Vector3(imageNode!.position.x + 0.1,
-                            imageNode!.position.y, imageNode!.position.z),
-                        imageNode!.scale,
-                      );
-                    }
-                  },
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back, size: 40),
+                      onPressed: () {
+                        if (imageNode != null) {
+                          updateNode(
+                            Vector3(imageNode!.position.x - 0.1,
+                                imageNode!.position.y, imageNode!.position.z),
+                            imageNode!.scale,
+                          );
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_upward, size: 40),
+                      onPressed: () {
+                        if (imageNode != null) {
+                          updateNode(
+                            Vector3(
+                                imageNode!.position.x,
+                                imageNode!.position.y + 0.1,
+                                imageNode!.position.z),
+                            imageNode!.scale,
+                          );
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_downward, size: 40),
+                      onPressed: () {
+                        if (imageNode != null) {
+                          updateNode(
+                            Vector3(
+                                imageNode!.position.x,
+                                imageNode!.position.y - 0.1,
+                                imageNode!.position.z),
+                            imageNode!.scale,
+                          );
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_forward, size: 40),
+                      onPressed: () {
+                        if (imageNode != null) {
+                          updateNode(
+                            Vector3(imageNode!.position.x + 0.1,
+                                imageNode!.position.y, imageNode!.position.z),
+                            imageNode!.scale,
+                          );
+                        }
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,4 @@
-import 'package:ar_flutter_plugin/datatypes/node_types.dart';
-import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
-import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/material.dart';
-import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
-
-import 'package:vector_math/vector_math_64.dart';
 
 import 'ar_screen.dart';
 
